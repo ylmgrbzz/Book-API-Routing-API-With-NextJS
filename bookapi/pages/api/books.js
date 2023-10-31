135df4dfc82e2e557c2726239149e145a0331b5b
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-10-31 13:06:32 +0300

    mongodb

--- bookapi/pages/api/books.js
+++ bookapi/pages/api/books.js
@@ -1,16 +1,32 @@
-import fs from "fs";
-import path from "path";
+// import fs from "fs";
+// import path from "path";
+import mongodb from "mongodb";
+// function getData() {
+//   const filePath = path.join(process.cwd(), "data", "books.json");
+//   const fileData = fs.readFileSync(filePath);
+//   const data = JSON.parse(fileData);
+//   return data;
+// }
 
-function getData() {
-  const filePath = path.join(process.cwd(), "data", "books.json");
-  const fileData = fs.readFileSync(filePath);
-  const data = JSON.parse(fileData);
-  return data;
-}
+export default async function handler(req, res) {
+  const client = await mongodb.MongoClient.connect(process.env.MONGODB_URI);
+  const db = client.db();
 
-export default function handler(req, res) {
   if (req.method === "GET") {
-    getData();
+    let booksCollection;
+    try {
+      const booksCollection = db.collection("books").find().sort().toArray();
+    } catch (error) {
+      console.error("Error:", error);
+    }
+    if (!booksCollection) {
+      return res.status(404).json({ message: "No books found" });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Books fetched", books: booksCollection });
+    // getData();
   } else if (req.method === "POST") {
     const { imgUrl, name, description } = req.body;
     const newBook = {
@@ -19,10 +35,12 @@ export default function handler(req, res) {
       name,
       description,
     };
-    const filePath = path.join(process.cwd(), "data", "books.json");
-    const data = getData();
-    data.push(newBook);
-    fs.writeFileSync(filePath, JSON.stringify(data));
+    const generateBooks = await db.collection("books").insertOne(newBook);
+
+    // const filePath = path.join(process.cwd(), "data", "books.json");
+    // const data = getData();
+    // data.push(newBook);
+    // fs.writeFileSync(filePath, JSON.stringify(data));
     return res
       .status(201)
       .json({ message: "Book added successfully!", book: newBook });
