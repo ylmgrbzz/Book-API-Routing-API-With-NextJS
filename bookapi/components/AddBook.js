a702e94f7cfa7f34033ac488c98cd6cba644b2db
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-10-31 11:33:46 +0300

    addbooks

--- bookapi/components/AddBook.js
+++ bookapi/components/AddBook.js
@@ -1,14 +1,47 @@
-import React from "react";
-import styles from "../styles/Form.module.css"
+import React, { useState } from "react";
+import styles from "../styles/Form.module.css";
 
 const AddBook = () => {
+  const [inputs, setInputs] = useState({
+    name: "",
+    description: "",
+    imgUrl: "",
+  });
+
+  const handleChange = (e) => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+  };
   return (
-    <div className={styles.container}   >  
-      <form  className={styles.formControl}     >
-        <label htmlFor="name"> name</label> <input type="text" name="name" />
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit} className={styles.formControl}>
+        <label htmlFor="name"> name</label>{" "}
+        <input
+          onChange={handleChange}
+          value={inputs.name}
+          type="text"
+          name="name"
+        />
         <label htmlFor="description"> description</label>{" "}
-        <input type="text" name="description" />
-        <label htmlFor="imgUrl"> imgUrl</label> <input type="text" name="imgUrl" />
+        <input
+          onChange={handleChange}
+          value={inputs.description}
+          type="text"
+          name="description"
+        />
+        <label htmlFor="imgUrl"> imgUrl</label>{" "}
+        <input
+          onChange={handleChange}
+          value={inputs.imgUrl}
+          type="text"
+          name="imgUrl"
+        />
         <button type="submit">Add Book</button>
       </form>
     </div>
