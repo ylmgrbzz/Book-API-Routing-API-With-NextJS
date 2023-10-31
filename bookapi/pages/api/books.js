f2a500051da1b39338bbfc2cae92ce92c4fa08cf
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-10-31 11:56:25 +0300

    booksapi

--- bookapi/pages/api/books.js
+++ bookapi/pages/api/books.js
@@ -1,34 +1,27 @@
 import fs from "fs";
 import path from "path";
 
-function getData() {
-  const filePath = path.join(process.cwd(), "data", "books.json");
-  const fileData = fs.readFileSync(filePath);
-  const data = JSON.parse(fileData);
-  res.status(200).json({ message: data });
-}
-
-function postData(req, res) {
-  const { imgUrl, name, description } = req.body;
-  const newBook = {
-    id: Math.random().toString(),
-    imgUrl,
-    name,
-    description,
-  };
-  const filePath = path.join(process.cwd(), "data", "books.json");
-  const data = getData();
-  data.push(newBook);
-  fs.writeFileSync(filePath, JSON.stringify(data));
-  return res
-    .status(201)
-    .json({ message: "Book added successfully!", book: newBook });
-}
 
 export default function handler(req, res) {
   if (req.method === "GET") {
-    getData();
+    const filePath = path.join(process.cwd(), "data", "books.json");
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData);
+    res.status(200).json({ message: data });
   } else if (req.method === "POST") {
-    postData();
+    const { imgUrl, name, description } = req.body;
+    const newBook = {
+      id: Math.random().toString(),
+      imgUrl,
+      name,
+      description,
+    };
+    const filePath = path.join(process.cwd(), "data", "books.json");
+    const data = getData();
+    data.push(newBook);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    return res
+      .status(201)
+      .json({ message: "Book added successfully!", book: newBook });
   }
 }
